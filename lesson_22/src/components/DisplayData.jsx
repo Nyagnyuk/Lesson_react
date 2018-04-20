fc50ada5313959d78da72b106c5a5d8bd7c2982d
Vinokur Natalya <nyagnyuk@ukr.net>
2018-04-20 17:48:49 +0300

    v.20.04.2

--- lesson_22/src/components/DisplayData.jsx
+++ lesson_22/src/components/DisplayData.jsx
@@ -5,9 +5,9 @@ export const DisplayData = (props) => {
 
 	return(
 		<div>
-			<h3 onClick={ (type: 'INC_COUNTER') => dispatch( {}, props.app)  
+			<h3 onClick={ () => dispatch( {type: 'INC_COUNTER'}, props.app)  
 		} >Counter: { props.app.state.counter }</h3>
-			<h3 onClick={ (type: 'ADD_ITEM') => dispatch( {}, props.app) 
+			<h3 onClick={ () => dispatch( {type: 'ADD_ITEM'}, props.app) 
 		} >Items: { props.app.state.items.join(',') }</h3>
 		</div>
 		)
--- lesson_22/src/reducer.js
+++ lesson_22/src/reducer.js
@@ -12,21 +12,36 @@
 // prepareState('CHANGE_COMMENT', author, id, hidden, content)
 
 
-export function dispatch( info, appObject ){
+export function dispatch( info, app ){
 	// let state = Object.assign({}, app.state);
 
-	let state = ( ..app.state); //получаем копию
+	// let state = { ...app.state}; //получаем копию
+	let state = {...app.state};
+	// state.items = [ ...app.state.items];
+
+	// let state = { };
+	// state.counter = app.state.counter;
+	// state.items = [];
+	// state.items[0] = app.state.items[0]
 
 	if( info.type === 'ADD_ITEM'){
 		let currentLenght = app.state.items.length;
-		state.items.push(currentLenght + 1)
+		// state.items.push(currentLenght + 1)
+
+		let value = currentLenght + 1;
+		state.items = [...state.items, value];
 		
 	}else if (info.type === 'INC_COUNTER'){
-		if(state.counter % 5){
-			
+		if((state.counter+1) % 5){ //если не кратное 5
+			// state.counter++;
+			state = {...state};//получили копию, потом изменяем
+			state.counter++;
+		}else{
+			state = {...state};
+			state.counter +=2;
 		}
 	}else{
-
+ 
 	}
 
 	app.setState(state);
